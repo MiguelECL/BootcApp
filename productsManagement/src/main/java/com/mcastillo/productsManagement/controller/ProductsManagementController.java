e347730480cc8b0de154324c9fdffa683c24456c
Miguel Castillo <mcastillo@192.168.1.8>
2025-04-07 15:54:05 -0700

    change from string to response entities in controller

--- productsManagement/src/main/java/com/mcastillo/productsManagement/controller/ProductsManagementController.java
+++ productsManagement/src/main/java/com/mcastillo/productsManagement/controller/ProductsManagementController.java
@@ -4,61 +4,64 @@ import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mcastillo.Product;
 import com.mcastillo.productsManagement.service.ProductsManagementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.TimeoutException;
 
 @RestController
+// @CrossOrigin annotation not really needed anymore due to the fact that the frontend is now
+// being served by springboot
 @CrossOrigin
 public class ProductsManagementController {
 
-  ProductsManagementService service = new ProductsManagementService();
+  // Inject the products management service, @Autowired is used to inject the service to let spring;s
+  // manage the lifecycle of the service
+  @Autowired
+  ProductsManagementService service;
 
   @GetMapping("/products")
-  public String getProducts() {
-
+  public ResponseEntity<?> getProducts() {
     try {
-      return service.getProducts();
+      return ResponseEntity.ok(service.getProducts());
     } catch (TimeoutException e){
-      System.out.println("TimeoutException: " + e.getMessage());
-      return "TimeoutException: " + e.getMessage();
+      return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("TimeoutException: " + e.getMessage());
     }
   }
 
   @PostMapping("/products")
-  public String createProduct(@RequestBody Product product) {
+  public ResponseEntity<?> createProduct(@RequestBody Product product) {
     try {
       service.createProduct(product);
-      return "Product created";
+      return ResponseEntity.ok("Product created with id: " + product.getId());
     } catch (TimeoutException e){
-      System.out.println("TimeoutException: " + e.getMessage());
-      return "TimeoutException: " + e.getMessage();
+      return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("TimeoutException: " + e.getMessage());
     } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("TimeoutException: " + e.getMessage());
     }
   }
 
   @PutMapping("/products/{id}")
-  public String updateProduct(@PathVariable String id, @RequestBody Product product) {
+  public ResponseEntity<?> updateProduct(@PathVariable String id, @RequestBody Product product) {
     try {
       service.updateProduct(product);
+      return ResponseEntity.ok("Product updated with id: " + id);
     } catch (TimeoutException e){
-      System.out.println("TimeoutException: " + e.getMessage());
-      return "TimeoutException: " + e.getMessage();
+      return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("TimeoutException: " + e.getMessage());
     } catch (JsonProcessingException e) {
-      throw new RuntimeException(e);
+      return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("TimeoutException: " + e.getMessage());
     }
-    return "Product updated with id: " + id;
   }
 
   @DeleteMapping("/products/{id}")
-  public String deleteProduct(@PathVariable int id) {
+  public ResponseEntity<?> deleteProduct(@PathVariable int id) {
     try {
       service.deleteProduct(id);
-      return "Product deleted with id: " + id;
+      return ResponseEntity.ok("Product deleted with id: " + id);
     } catch (TimeoutException e){
-      System.out.println("TimeoutException: " + e.getMessage());
-      return "Error Deleting Product";
+      return ResponseEntity.status(HttpStatus.REQUEST_TIMEOUT).body("TimeoutException: " + e.getMessage());
     }
   }
 }
