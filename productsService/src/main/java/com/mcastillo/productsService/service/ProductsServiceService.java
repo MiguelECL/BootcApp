a8bc95989a9c52baf4f5a7becc8e72c90c378033
Miguel Castillo <mcastillo@192.168.1.8>
2025-04-08 14:06:09 -0700

    refactor to ProductsServiceService to allow for better testing

--- productsService/src/main/java/com/mcastillo/productsService/service/ProductsServiceService.java
+++ productsService/src/main/java/com/mcastillo/productsService/service/ProductsServiceService.java
@@ -2,7 +2,6 @@ package com.mcastillo.productsService.service;
 
 import com.amazonaws.services.sqs.*;
 import com.amazonaws.services.sqs.model.Message;
-import com.mcastillo.productsService.configuration.Queries;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.amazonaws.services.sqs.model.DeleteMessageRequest;
@@ -11,54 +10,83 @@ import com.mcastillo.productsService.repository.ProductsServiceRepository;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /// This service receives messages from products Management through SQS and communicates with the database.
 @Service
 public class ProductsServiceService {
 
-    private final String queueURL = System.getenv("QUEUE_URL");
-    private final AmazonSQS sqsClient = AmazonSQSClientBuilder.defaultClient();
-    private final AmazonSQSResponder sqsResponder = AmazonSQSResponderClientBuilder.defaultClient();
-
-    // Inject repository service
-    @Autowired
-    private ProductsServiceRepository repository;
+    private final String queueURL;
+    private final AmazonSQS sqsClient;
+    private final AmazonSQSResponder sqsResponder;
+    private final ProductsServiceRepository repository;
 
     @Autowired
-    private Queries queries;
+    public ProductsServiceService(ProductsServiceRepository repository){
+        this.repository = repository;
+        this.sqsClient = AmazonSQSClientBuilder.defaultClient();
+        this.sqsResponder = AmazonSQSResponderClientBuilder.defaultClient();
+        this.queueURL = System.getenv("QUEUE_URL");
+    }
 
-    // Continuously poll for messages from the queue
+    // Constructor for testing
+    public ProductsServiceService(ProductsServiceRepository repository,
+                                  AmazonSQS sqsClient,
+                                  AmazonSQSResponder sqsResponder,
+                                  String queueURL) {
+        this.repository = repository;
+        this.sqsClient = sqsClient;
+        this.sqsResponder = sqsResponder;
+        this.queueURL = queueURL;
+    }
+
+    private final ExecutorService executorService = Executors.newFixedThreadPool(10);
+
     @PostConstruct
+    public void initializePolling(){
+        new Thread(this::pollQueueContinuously).start();
+    }
+
+    protected void pollQueueContinuously() {
+        while (true) {
+            pollQueue();
+        }
+    }
+
     public void pollQueue() {
         System.out.println("Polling SQS...");
 
-        new Thread(() -> {
-            while (true) {
-                ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
-                        .withQueueUrl(queueURL)
-                        .withMessageAttributeNames("All")
-                        .withMaxNumberOfMessages(5)
-                        .withWaitTimeSeconds(5);
+        ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest()
+                .withQueueUrl(queueURL)
+                .withMessageAttributeNames("All")
+                .withMaxNumberOfMessages(5)
+                .withWaitTimeSeconds(20);
 
-                List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
-                System.out.println(messages);
+        List<Message> messages = sqsClient.receiveMessage(receiveMessageRequest).getMessages();
+        System.out.println(messages);
 
-                for (Message message : messages){
+
+        for (Message message : messages){
+            executorService.submit(() -> {
+                try {
                     System.out.println("Received message: " + message.getBody());
                     processMessage(message);
                     deleteMessage(message);
+                } catch (Exception e) {
+                    System.out.println("Error processing message: " + e.getMessage());
+                    e.printStackTrace();
                 }
-            }
-        }).start();
+            });
+        }
     }
 
     public void processMessage(Message message) {
-        // Process the message
         String response = repository.executeQuery(message);
         sqsResponder.sendResponseMessage(MessageContent.fromMessage(message),new MessageContent(response));
     }
 
-    private void deleteMessage(Message message) {
+    public void deleteMessage(Message message) {
         DeleteMessageRequest deleteMessageRequest = new DeleteMessageRequest()
                 .withQueueUrl(queueURL)
                 .withReceiptHandle(message.getReceiptHandle());
