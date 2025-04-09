780a53f660a20ed73a1f4a261f8ffc0e437ffed4
Miguel Castillo <mcastillo@192.168.1.8>
2025-04-09 15:11:28 -0700

    removed logger from interface

--- productsManagement/src/main/java/com/mcastillo/productsManagement/service/ProductsManagementService.java
+++ productsManagement/src/main/java/com/mcastillo/productsManagement/service/ProductsManagementService.java
@@ -2,14 +2,10 @@ package com.mcastillo.productsManagement.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mcastillo.Product;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeoutException;
 
 public interface ProductsManagementService {
-
-	Logger logger = LoggerFactory.getLogger("logger");
 
 	String getProducts() throws TimeoutException;
 	String createProduct(Product product) throws TimeoutException, JsonProcessingException;
